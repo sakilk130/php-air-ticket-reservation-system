d5368b723a118037ca8544a029003219bfe62bf6
sakilk130 <sakilk130@gmail.com>
2020-03-11 22:39:06 +0600

    add navbar.html

--- user/uchangepassword.php
+++ user/uchangepassword.php
@@ -21,48 +21,9 @@
   </head>
   <link rel="stylesheet" href="css/uchangepassword.css" />
   <body>
-    <section id="navbar">
-      <div class="container-fluid">
-        <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
-          <!-- Brand -->
-          <a class="navbar-brand" href="/Mid-Project/index.php">Bangladesh Airlines</a>
-
-          <!-- Links -->
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="udashboard.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-          </ul>
-          <!-- Dropdown -->
-          <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbardrop"
-                  data-toggle="dropdown"
-                >
-                  Hi,<?php echo $_SESSION['loggedinuser'];?>
-                </a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="ubookflight.php">Booking</a>
-                  <a class="dropdown-item" href="unotice.php">Notice</a>
-                  <a class="dropdown-item" href="uchangepassword.php"
-                    >Change Password</a
-                  >
-                  <a class="dropdown-item" href="usettings.php">Settings</a>
-                  <a class="dropdown-item" href="Login.php">Logout</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </section>
+  <?php
+   include("navbar.html"); 
+   ?>
     <section id="usettings">
       <div class="">
         <div class="login p-l-55 p-r-55 p-t-65 p-b-50">
